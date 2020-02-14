43a85ea5c242e01deb81f16f91fe52498606f081
Edward Wang <edward.yu.wang@gmail.com>
2020-02-15 02:58:34 +0800

    First attempt on algebraic effects

--- LambdaCalculus/spago.dhall
+++ LambdaCalculus/spago.dhall
@@ -10,6 +10,7 @@ You can edit this file as you like.
     , "ordered-collections"
     , "parsing"
     , "psci-support"
+    , "run"
     , "test-unit"
     ]
 , packages = ./packages.dhall
--- LambdaCalculus/src/Main.purs
+++ LambdaCalculus/src/Main.purs
@@ -5,8 +5,13 @@ import Data.Either (Either(..))
 import Data.Map as M
 import Effect (Effect)
 import Effect.Console (log, logShow)
-import LambdaCalculus (LambdaLine(..), Env, line, norm)
+import LambdaCalculus (Env, LambdaLine(..), Term, line, norm)
 import Node.ReadLine as RL
+import Run (FProxy, Run, SProxy(..))
+import Run as Run
+import Run.Except (EXCEPT, throw)
+import Run.State (STATE, get, modify)
+import Run.Writer (WRITER, tell)
 import Text.Parsing.Parser (runParser)
 
 repl :: RL.Interface -> Env -> RL.LineHandler Unit
@@ -33,3 +38,36 @@ main = do
   console <- RL.createConsoleInterface RL.noCompletion
   RL.setPrompt "λ> " 3 console
   prompt console $ repl console M.empty
+
+---
+
+data LambdaREPLF a
+  = ParseLine String a
+
+derive instance functorLambdaREPLF :: Functor LambdaREPLF
+
+type LAMBDAREPL = FProxy LambdaREPLF
+
+_lambdarepl = SProxy :: SProxy "lambdarepl"
+
+parseLine :: forall r. String -> Run (lambdarepl :: LAMBDAREPL | r) Unit
+parseLine s = Run.lift _lambdarepl $ ParseLine s unit
+
+type ReplEffects r =
+  ( lambdarepl :: LAMBDAREPL
+  , state :: STATE Env
+  , writer :: WRITER Term
+  , except :: EXCEPT String | r
+  )
+
+_repl :: forall r. String -> Run (ReplEffects r) Unit
+_repl s = do
+  case runParser s line of
+    Left err ->
+      throw $ "parse error: " <> show err
+    Right Blank -> pure unit
+    Right (Run t) -> do
+      env <- get
+      tell $ norm env t
+    Right (Let v t) ->
+      modify $ M.insert v t
