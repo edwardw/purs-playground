584e2de52ceb1669a7c05f6fb64ffcaa15f8f9fb
Edward Wang <edward.yu.wang@gmail.com>
2020-03-30 22:39:53 +0800

    Array was the fastest for membership check of fixed length set

--- charset/benchmarks/Main.purs
+++ charset/benchmarks/Main.purs
@@ -4,13 +4,20 @@ import Prelude
 import Benchotron.Core (Benchmark, benchFn, benchFn', mkBenchmark)
 import Benchotron.UI.Console (runSuite)
 import Data.Array ((..))
+import Data.Array as A
+import Data.ArrayBuffer.Typed as ArrayBuffer
+import Data.ArrayBuffer.Types (Uint8Array)
 import Data.Foldable (foldr)
 import Data.HashMap as HashMap
+import Data.HashSet as HashSet
 import Data.IntMap as IntMap
 import Data.Map as Map
+import Data.Maybe (isJust)
 import Data.Profunctor.Strong ((***))
-import Data.Tuple (Tuple(..))
+import Data.Tuple (Tuple(..), uncurry)
+import Data.UInt as UInt
 import Effect (Effect)
+import Effect.Unsafe (unsafePerformEffect)
 import Test.QuickCheck.Arbitrary (arbitrary)
 import Test.QuickCheck.Gen (Gen, vectorOf)
 
@@ -23,9 +30,9 @@ benchInsert = mkBenchmark
   , sizeInterpretation: "Number of elements to insert"
   , inputsPerSize: 1
   , gen: \n -> vectorOf n arbitrary :: Gen (Array (Tuple Int String))
-  , functions: [ benchFn "Map.insert" (foldr (\(Tuple k v) -> Map.insert k v) Map.empty)
-               , benchFn "IntMap.insert" (foldr (\(Tuple k v) -> IntMap.insert k v) IntMap.empty)
-               , benchFn "HashMap.insert" (foldr (\(Tuple k v) -> HashMap.insert k v) HashMap.empty)
+  , functions: [ benchFn "Map.insert" (foldr (uncurry Map.insert) Map.empty)
+               , benchFn "IntMap.insert" (foldr (uncurry IntMap.insert) IntMap.empty)
+               , benchFn "HashMap.insert" (foldr (uncurry HashMap.insert) HashMap.empty)
                ]
   }
 
@@ -39,13 +46,13 @@ benchDelete = mkBenchmark
   , inputsPerSize: 1
   , gen: \n -> vectorOf n arbitrary :: Gen (Array (Tuple Int String))
   , functions: [ benchFn' "Map.delete"
-                          (\m -> Map.keys m # foldr (\k -> Map.delete k) m)
+                          (\m -> Map.keys m # foldr Map.delete m)
                           Map.fromFoldable
                , benchFn' "IntMap.delete"
-                          (\m -> IntMap.indices m # foldr (\k -> IntMap.delete k) m)
+                          (\m -> IntMap.indices m # foldr IntMap.delete m)
                           IntMap.fromAssocArray
                , benchFn' "HashMap.delete"
-                          (\m -> HashMap.keys m # foldr (\k -> HashMap.delete k) m)
+                          (\m -> HashMap.keys m # foldr HashMap.delete m)
                           HashMap.fromFoldable
                ]
   }
@@ -64,17 +71,42 @@ benchUnion = mkBenchmark
           let res = Tuple left right :: Tuple (Array (Tuple Int String)) (Array (Tuple Int String))
           pure res
   , functions: [ benchFn' "Map.union"
-                          (\(Tuple m1 m2) -> Map.union m1 m2)
+                          (uncurry Map.union)
                           (Map.fromFoldable *** Map.fromFoldable)
                , benchFn' "IntMap.unionLeft"
-                          (\(Tuple m1 m2) -> IntMap.unionLeft m1 m2)
+                          (uncurry IntMap.unionLeft)
                           (IntMap.fromAssocArray *** IntMap.fromAssocArray)
                , benchFn' "HashMap.union"
-                          (\(Tuple m1 m2) -> HashMap.union m1 m2)
+                          (uncurry HashMap.union)
                           (HashMap.fromFoldable *** HashMap.fromFoldable)
                ]
   }
 
 
+benchMember :: Benchmark
+benchMember = mkBenchmark
+  { slug: "membership"
+  , title: "check membership of a set"
+  , sizes: [8, 16, 32, 64, 128]
+  , sizeInterpretation: "Number of elements in the set"
+  , inputsPerSize: 1
+  , gen: \n -> vectorOf n (flip mod n <$> arbitrary) :: Gen (Array Int)
+  , functions: [ benchFn' "HashSet.member"
+                          (\(Tuple a s) -> flip HashSet.member s <$> a)
+                          (\a -> Tuple a (HashSet.fromFoldable a))
+               , benchFn "Array"
+                         (\a -> isJust <$> A.index a <$> a)
+               , benchFn' "ArrayBuffer"
+                          (\(Tuple a arr) ->
+                            isJust <$> (\i -> unsafePerformEffect (ArrayBuffer.at arr i)) <$> a
+                          )
+                          (\a ->
+                            let arr = (unsafePerformEffect (ArrayBuffer.fromArray $ map UInt.fromInt a)) :: Uint8Array
+                            in Tuple a arr
+                          )
+               ]
+  }
+
+
 main :: Effect Unit
-main = runSuite [benchInsert, benchDelete, benchUnion]
+main = runSuite [benchInsert, benchDelete, benchUnion, benchMember]
